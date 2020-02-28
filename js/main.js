7dcc98f5909e88f1e2d9958602d49d93fa0dbd73
Raffaele Riviello <raffaele.riviello@gmail.com>
2020-02-28 18:42:58 +0100

    intial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// $(".hamburger-bars").click(function(){
+//   $(".hamburger-menu").addClass("active");
+// });
+//
+// $(".close").click(function(){
+//   $(".hamburger-menu").removeClass("active");
+// });
+
+$(".header-right > a , .close").click(function() { $(".hamburger-menu").toggleClass("active");});
